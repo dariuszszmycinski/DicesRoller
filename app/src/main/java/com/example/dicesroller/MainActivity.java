7359764847cfffae24ace47dd5681b9fa0154e99
Daro <szmytek89@gmail.com>
2023-10-26 10:57:28 +0200

    Roll a single d6

--- app/src/main/java/com/example/dicesroller/MainActivity.java
+++ app/src/main/java/com/example/dicesroller/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.dicesroller;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView rollResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton = findViewById(R.id.rollButton);
+        rollResult = findViewById(R.id.rollResult);
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pressButton();
+            }
+        });
+    }
+
+    private void pressButton() {
+        rollResult.setText(String.valueOf(rollD6()));
+    }
+
+    private int rollD6() {
+        int min = 1;
+        int max = 6;
+        return (int)Math.floor(Math.random() * (max - min + 1) + min);
     }
 }
