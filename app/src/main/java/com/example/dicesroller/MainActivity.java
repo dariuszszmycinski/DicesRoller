1c8f3b972ee246f35f002670852fa11c6ce987a2
Daro <szmytek89@gmail.com>
2023-11-17 11:36:49 +0100

    Modificator button added

--- app/src/main/java/com/example/dicesroller/MainActivity.java
+++ app/src/main/java/com/example/dicesroller/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     Button diceTypeButton;
     Button dicesQuantity;
 
+    Button modificatorButton;
+
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         diceTypeButton = findViewById(R.id.diceTypeButton);
         rollHistory = findViewById(R.id.resultsHistoryLabel);
         dicesQuantity = findViewById(R.id.dicesQuantityButton);
+        modificatorButton = findViewById(R.id.modificatorButton);
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -106,6 +109,41 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        modificatorButton.setOnTouchListener(new View.OnTouchListener() {
+            private float mPreviousY;
+            boolean mIsDown;
+            @Override
+            public boolean onTouch(View view, MotionEvent e) {
+                float y ;
+                float dy = 0;
+
+                switch (e.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        mIsDown = true;
+                        mPreviousY = e.getY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        if (mIsDown){
+                            y = e.getY();
+                            dy = y - mPreviousY;
+                        }
+                    case MotionEvent.ACTION_UP:
+                        if (mIsDown){
+                            if (dy<=0){
+                                int quantity = Integer.parseInt(String.valueOf(modificatorButton.getText()))+1;
+                                modificatorButton.setText(String.valueOf(quantity));
+                            }else {
+                                int quantity = Integer.parseInt(String.valueOf(modificatorButton.getText()))-1;
+                                modificatorButton.setText(String.valueOf(quantity));
+                            }
+                        }
+                        mIsDown = false;
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     private void pressRoll() {
@@ -114,17 +152,22 @@ public class MainActivity extends AppCompatActivity {
 
     private int rollDice() {
         int qty = Integer.parseInt(String.valueOf(dicesQuantity.getText()));
+        int mod = Integer.parseInt(String.valueOf(modificatorButton.getText()));
         int min = 1;
         int max = diceType.getSides();
         int resultSum = 0;
         StringBuilder sb = new StringBuilder();
-        sb.append(rollHistory.getText()).append(", ").append(qty).append(diceType).append(": ");
+        sb.append(rollHistory.getText()).append(qty).append(diceType);
+        if (mod>0) sb.append("+").append(mod);
+        if (mod<0) sb.append(mod);
+        sb.append(": ");
         for (int i = 0; i < qty; i++) {
             int result = (int) (Math.floor(Math.random() * (max - min + 1) + min));
             resultSum += result;
             sb.append(result).append(", ");
         }
-        sb.append("total = ").append(resultSum).append(". ");
+        resultSum += mod;
+        sb.append("total = ").append(resultSum).append("\n");
         rollHistory.setText(sb.toString());
         return resultSum;
     }
